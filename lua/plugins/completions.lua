d5e63e91e9fe73fb93dc9668822f5276d4d5c93c
cuban-goats <erikkoston@mailfence.com>
2025-07-31 23:18:36 +0200

    added jsregexp library

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,23 +1,25 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
 		"L3MON4D3/LuaSnip",
+    version = "<CurrentMajor>.*",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
+			build = "make install_jsregexp",
 		},
 	},
 	{
-    "hrsh7th/nvim-cmp",
+		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
-          expand = function(args)
+					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
