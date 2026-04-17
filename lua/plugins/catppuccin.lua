b6c98c2eaecfc9ab4bb1a344d08d96c1f25cb2fb
I767435 <erik.koston@sap.com>
2026-04-17 18:13:44 +0200

    theme change

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,8 @@
-return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function ()
-    vim.cmd.colorscheme "catppuccin"
-  end
-}
+  return {
+    "ku1ik/vim-monokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("monokai")
+    end,
+  }
