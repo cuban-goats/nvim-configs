0e6fb7bd6a9dca18d987f63bfe5cbe6dbb947e8b
cuban-goats <erikkoston@mailfence.com>
2026-04-25 13:20:57 +0200

    fixed treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,18 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    version = false,
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter").setup()
+      require("nvim-treesitter.configs").setup({
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
 
-      -- Enable syntax highlighting via native treesitter
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function(args)
-          pcall(vim.treesitter.start, args.buf)
-        end,
-      })
-
-      -- Auto-install parser for the current filetype
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function()
-          local ft = vim.bo.filetype
-          if ft == "" then return end
-          local installed = require("nvim-treesitter.config").get_installed()
-          if not vim.list_contains(installed, ft) then
-            local available = require("nvim-treesitter.config").get_available()
-            if vim.list_contains(available, ft) then
-              require("nvim-treesitter.install").install({ ft })
-            end
-          end
-        end,
+        -- 👇 THIS is the modern auto-install switch
+        auto_install = true,
       })
     end,
   },
