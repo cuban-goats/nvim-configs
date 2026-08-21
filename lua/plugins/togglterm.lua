9f6cccfa9543ee32d88732849bd99c7b35780dac
I767435 <erik.koston@sap.com>
2026-08-21 11:39:49 +0200

    updated toggleterm config

--- lua/plugins/togglterm.lua
+++ lua/plugins/togglterm.lua
@@ -2,28 +2,14 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   opts = {
-    direction = "float",
-    float_opts = {
-      border = "curved",
-      width = function()
-        return vim.o.columns
-      end,
-      height = function()
-        return math.floor(vim.o.lines * 0.45)
-      end,
-      row = function()
-        return math.floor(vim.o.lines * 0.50)
-      end,
-      col = 0,
-      winblend = 10,
-    },
+    direction = "vertical",
+    size = function()
+      return math.floor(vim.o.columns * 0.4)
+    end,
     hide_numbers = true,
     shade_terminals = true,
     shading_factor = 2,
     start_in_insert = true,
-    highlights = {
-      FloatBorder = { link = "FloatBorder" },
-    },
   },
   keys = {
     { "<leader>tt", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
